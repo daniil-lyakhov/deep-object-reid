9978ae04ccabf8f6e76ee3e5cad52d9740a9dfbe
daniil-lyakhov <daniil.lyakhov@intel.com>
2021-09-03 14:37:24 +0300

    Add nncf training configs

---
+++ training/efficientnet/compress.sh
@@ -0,0 +1,9 @@
+python /home/automation/dlyakhov/training_extensions/external/deep-object-reid/tools/main.py \
+--config-file /home/automation/dlyakhov/training_extensions/external/deep-object-reid/training/efficientnet/EfficientNet_b0.yml \
+--gpu-num 1 \
+--custom-roots \
+/home/automation/eegupova/ote/datasets/CIFAR100/train \
+/home/automation/eegupova/ote/datasets/CIFAR100/val \
+--root _ \
+model.load_weights /mnt/icv_externalN/dlyakhov/ote-classification-checkpoint/CIFAR100_efficientnet/{name}.pth.tar-149 \
+data.save_dir /home/automation/dlyakhov/training_extensions/external/deep-object-reid/training/efficientnet/output
---
+++ training/mobilenet_v3_large_1/compress.sh
@@ -0,0 +1,9 @@
+python /home/automation/dlyakhov/training_extensions/external/deep-object-reid/tools/main.py \
+--config-file /home/automation/dlyakhov/training_extensions/external/deep-object-reid/training/mobilenet_v3_large_1/mobilenetv3_large.yml \
+--gpu-num 1 \
+--custom-roots \
+/home/automation/eegupova/ote/datasets/CIFAR100/train \
+/home/automation/eegupova/ote/datasets/CIFAR100/val \
+--root _ \
+model.load_weights /mnt/icv_externalN/dlyakhov/ote-classification-checkpoint/CIFAR100_mobielenet_v3_large/model_0/{name}.pth.tar-113 \
+data.save_dir /home/automation/dlyakhov/training_extensions/external/deep-object-reid/training/mobilenet_v3_large_1/output
