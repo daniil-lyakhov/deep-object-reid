7c32133c3d53c251459d4a65dd3e266706d47e9a
daniil-lyakhov <daniil.lyakhov@intel.com>
2021-09-24 05:22:56 -0400

    Updated configs

--- training/pruning_int8/mobilenet_v3_large_1/compress.sh
+++ training/pruning_int8/mobilenet_v3_large_1/compress.sh
@@ -6,4 +6,4 @@ python /home/automation/dlyakhov/training_extensions/external/deep-object-reid/t
 /home/automation/dlyakhov/training_extensions/external/deep-object-reid/CIFAR100/val \
 --root _ \
 model.load_weights /mnt/icv_externalN/dlyakhov/ote-classification-checkpoint/CIFAR100_mobielenet_v3_large/model_0/{name}.pth.tar-113 \
-data.save_dir /home/automation/dlyakhov/training_extensions/external/deep-object-reid/training/pruning_int8/mobilenet_v3_large_1/output
+data.save_dir /home/automation/dlyakhov/training_extensions/external/deep-object-reid/training/pruning_int8/mobilenet_v3_large_1/output_21_09
